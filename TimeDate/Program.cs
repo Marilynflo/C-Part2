346710fdd6e5b74ca8bc268a72490b444556d919
mmf09 <mmf09@YPC>
2022-12-15 21:22:54 -0800

    Add project files.

---
+++ TimeDate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TimeDateAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            string format = "MMMM d yyyy hh:mm tt";
+            Console.WriteLine("Today's date and time is: ");
+            Console.WriteLine(time.ToString(format));
+            Console.WriteLine("Please enter a number: ");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The date and time after your entry is: ");
+            Console.WriteLine(DateTime.Now.AddHours(hour));
+            Console.ReadLine();
+        }
+    }
+}
