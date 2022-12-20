5927cc443ab000f1976ab730dbc87859a69c3e07
mmf09 <mmf09@YPC>
2022-12-19 21:15:09 -0800

    Add project files.

---
+++ Method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methods math = new methods();
+            Console.WriteLine("Hello, Please enter your favorite number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The number you entered minus 2 = " + math.mathOperation2(input));
+            Console.WriteLine("The number you entered times 3 = " + math.mathOperation3(input));
+            Console.WriteLine("The number you entered add 4 = " + math.mathOperation4(input));
+
+
+
+        }
+    }
+}
---
+++ Method/methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class methods
+    {
+        public int mathOperation2(int substractionNumber)
+        {
+            int total = substractionNumber - 2;
+            return total;
+        }
+        public int mathOperation3(int timesNumber)
+        {
+
+                  int total = timesNumber * 3;
+            return total;
+
+         }
+    public int mathOperation4(int addingNumber)
+        {
+            int total = addingNumber + 4;
+            return total;
+        }
+    }
+}
